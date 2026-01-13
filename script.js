0110f2543d2aa150e108da2dc7a6e8868eea6e2c
Jacob Stephen <Cashflow34steph@gmail.com>
2026-01-13 15:25:26 +0100

    Add finishing touches

--- script.js
+++ script.js
@@ -10,9 +10,6 @@ function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-addBookToLibrary("1984", "George Orwell", 328, true);
-
 function displayLibrary() {
   const container = document.querySelector("#Library_container");
   container.textContent = "";
@@ -44,10 +41,6 @@ function displayLibrary() {
     let label = document.createElement("label");
     let readStatus = book.read ? "Read" : "Not Read Yet";
     label.textContent = `${readStatus}`;
-
-    // let checkbox = document.createElement("input");
-    // checkbox.type = "checkbox";
-    // book.read ? (checkbox.checked = true) : (checkbox.checked = false);
 
     let changeCheckbtn = document.createElement("button");
     changeCheckbtn.classList.add("change");
@@ -88,10 +81,3 @@ submitBtn.addEventListener("click", (e) => {
   displayLibrary();
   dialog.close();
 });
-/**
- * Add a button on each book’s display to change its read status.
-  To facilitate this you will want to create Book prototype function
-   that toggles a book instance’s read status.
-  Add a change status button
-  when clicked the book changes status
- */
